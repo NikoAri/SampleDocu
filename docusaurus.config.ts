ccd2eacd85f3f160f4108dc1bb29ee499fda21b2
Nikolai Aristov <43590492+NikoAri@users.noreply.github.com>
2024-03-14 11:59:21 -0700

    Update image in blog post and modify site title and tagline

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Niko Site',
-  tagline: 'Dinosaurs are cool',
+  title: '🏄‍♂️ Niko 🏄‍♂️',
+  tagline: 'Surfing with Dinos',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
